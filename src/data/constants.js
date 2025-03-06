1f608c2a67f42bc7bee3cb3a8fbdb670aa41c900
Abhijeet Singh <122ad0010@iiitk.ac.in>
2025-03-06 23:19:10 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     "DevOPS Engineer",
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "I am a motivated and versatile individual, always ready to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/abhijeetGithu",
   resume:
     "https://drive.google.com/file/d/1uonSOai7jJjevn9lOx1TrwGAs35pugHY/view?usp=sharing",
